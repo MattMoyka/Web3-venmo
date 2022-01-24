f32ba546e70ea1d24e83319ad5c89d24b72ff642
mattmoyka <mattmoyka@gmail.com>
2022-01-24 17:14:52 -0500

    folder and file structure set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-
+import { Navbar, Welcome, Footer, Services, Transactions } from './components'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-     </h1>
+    <div className="min-h-screen">
+      <div class="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
