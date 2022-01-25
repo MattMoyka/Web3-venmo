2ff9e93e61b7801803444beb1cb2f6c61f81a2ec
mattmoyka <mattmoyka@gmail.com>
2022-01-25 17:14:31 -0500

    new transactions rendering on the page with gif

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,11 +1,12 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 contract Transactions {
     uint256 transactionCount;
 
     event Transfer(
-        address from,
+        address sender,
         address receiver,
         uint256 amount,
         string message,
@@ -41,6 +42,7 @@ contract Transactions {
                 keyword
             )
         );
+
         emit Transfer(
             msg.sender,
             receiver,
@@ -55,7 +57,9 @@ contract Transactions {
         public
         view
         returns (TransferStruct[] memory)
-    {}
+    {
+        return transactions;
+    }
 
     function getTransactionCount() public view returns (uint256) {
         return transactionCount;
