de54e410a7fdd54e47bc45646d569f21615b8cdf
mattmoyka <mattmoyka@gmail.com>
2022-01-25 14:20:55 -0500

    set up util file to fetch gifs from giphy

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -31,7 +31,9 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
         )}
 
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-
+          <p className="text-[#37c7da] font-bold">
+            {timestamp}
+          </p>
         </div>
       </div>
     </div>
